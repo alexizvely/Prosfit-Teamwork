df3bd1a650003ea67ec5d824f2e98f58f1276a81
alexizvely <alexizvely@gmail.com>
2016-04-10 20:47:52 +0300

    initial vely test commit

---
+++ pando-3d/static/javascripts/layout/layout.module.js
@@ -0,0 +1,11 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('pando-3d.layout', [
+            'pando-3d.layout.controllers'
+        ]);
+
+    angular
+        .module('pando-3d.layout.controllers', []);
+})();
