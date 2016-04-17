7498704d738f4f61692002199a3a72d2761c3c76
Boris Stoyanov <boris.stoyanovv@gmail.com>
2016-04-17 12:44:13 +0300

    Deployment commit with 3S configuration

--- pando3d/settings.py
+++ pando3d/settings.py
@@ -133,15 +133,14 @@ AWS_STORAGE_BUCKET_NAME = 'pando-3d-images'
 AWS_ACCESS_KEY_ID = 'AKIAI77TISWI3WD7C3ZQ'
 AWS_SECRET_ACCESS_KEY = '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'
 
-MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
 # Tell django-storages that when coming up with the URL for an item in S3 storage, keep
 # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
 # This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
 # We also use it in the next setting.
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
 # refers directly to STATIC_URL. So it's safest to always set it.
 # STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
