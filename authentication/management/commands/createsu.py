0031863ca6341743c003a18397efd8e8da8bfcaa
Boris Stoyanov <boris.stoyanovv@gmail.com>
2016-04-19 11:22:31 +0300

    added an if for the createsu command

--- authentication/management/commands/createsu.py
+++ authentication/management/commands/createsu.py
@@ -6,6 +6,6 @@ from authentication.models import Account
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        #i f not Account.objects.filter(username="admin").exists():
+        if not Account.objects.filter(username="admin").exists():
             Account.objects.create_superuser("admin@admin.admin", "adminadmin", username="admin")
 
