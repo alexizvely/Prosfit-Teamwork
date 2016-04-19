20a4fe2be6b5154aaa6ef0add43fd090752e46e4
GeriCookie <Gergana.bailova@gmail.com>
2016-04-18 20:09:57 -0400

    added svgModel to database and server

--- projects/models.py
+++ projects/models.py
@@ -3,6 +3,15 @@ from authentication.models import Account
 from pando3d.constants import STATUS_OPTIONS, SHAPES
 
 # Create your models here.
+def upload_file_to(instance, filename):
+    import os
+    from django.utils.timezone import now
+    filename_base, filename_ext = os.path.splitext(filename)
+    return 'models/%s%s' % (
+        now().strftime("%Y%m%d%H%M%S"),
+        filename_ext.lower(),
+    )
+
 
 class Project(models.Model):
     author = models.ForeignKey(Account)
@@ -16,6 +25,6 @@ class Project(models.Model):
     status = models.CharField(max_length=50, 
                             choices=STATUS_OPTIONS, 
                             default='saved')
-
+    svgFile = models.FileField(max_length=None, upload_to=upload_file_to, blank=True, default='text.svg')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- projects/serializers.py
+++ projects/serializers.py
@@ -5,11 +5,12 @@ from models import Project
 
 class ProjectSerializer(serializers.ModelSerializer):
     author = AccountSerializer(read_only=True, required=False)
+    svgFile = serializers.FileField(write_only=True, required=False)
 
     class Meta:
         model = Project
 
-        fields = ('id', 'name', 'shape_type', 'dimension_x', 'dimension_y', 'dimension_z', 'color', 'status', 'author')
+        fields = ('id', 'name', 'shape_type', 'dimension_x', 'dimension_y', 'dimension_z', 'color', 'status', 'author', 'created_at', 'updated_at', 'svgFile')
 
     def get_validation_exclusions(self, *args, **kwargs):
         exclusions = super(PostSerializer, self).get_validation_exclusions()
