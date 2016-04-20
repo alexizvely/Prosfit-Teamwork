feac9a91d8c2e490b1eb015284763a24feb6d90a
GeriCookie <Gergana.bailova@gmail.com>
2016-04-19 20:47:41 -0400

    I hope for a miracle

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from authentication.models import Account
 from pando3d.constants import STATUS_OPTIONS, SHAPES
+from pando3d.settings import AWS_STORAGE_BUCKET_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
 # Create your models here.
 def upload_file_to(instance, filename):
@@ -11,6 +12,28 @@ def upload_file_to(instance, filename):
         now().strftime("%Y%m%d%H%M%S"),
         filename_ext.lower(),
     )
+
+def push_picture_to_s3(filename):
+    try:
+        import os
+        import boto
+        from boto.s3.key import key
+
+        logging.getLogger('boto').setLevel(login.CRITICAL)
+        bucket_name = AWS_STORAGE_BUCKET_NAME
+        conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+        bucket = conn.get_bucket(bucket_name)
+        filename_base, filename_ext = os.path.splitext(filename)
+        key = '%s.%s' % filename_base, filename_ext.lower()
+        fn = 'models/%' % key
+        k = Key(bucket)
+        k.key = key
+        k.set_contents_from_filename(fn)
+        k.make_public()
+        os.remove(fn)
+        return "http://s#.amazonaws.com/%/%" % bucket_name, key
+    except:
+        return "error"
 
 
 class Project(models.Model):
@@ -26,6 +49,6 @@ class Project(models.Model):
                             choices=STATUS_OPTIONS, 
                             default='saved')
     svgText = models.CharField(max_length=10000000, default="")
-    svgFile = models.FileField(max_length=None, upload_to=upload_file_to, blank=True, default='text.svg')
+    svgFile = models.FileField(upload_to=push_picture_to_s3, blank=True, default='text.svg')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
